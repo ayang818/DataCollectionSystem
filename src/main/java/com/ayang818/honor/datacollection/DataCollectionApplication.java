4fd1c513e1856900290bc1e0003e2dd8b0d3e6ec
杨丰畅 <ayang818@qq.com>
2019-10-23 20:56:09 +0800

    init project && finish necessary configurations

---
+++ src/main/java/com/ayang818/honor/datacollection/DataCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.ayang818.honor.datacollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ayang818/honor/datacollection/DataCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayang818.honor.datacollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
