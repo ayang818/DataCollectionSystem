608bdf44143771c0f6e1bc946766c7e2d879bc16
杨丰畅 <ayang818@qq.com>
2019-10-25 16:48:47 +0800

    finish login feature && start doing admin server

---
+++ src/main/java/com/ayang818/honor/datacollection/conf/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.ayang818.honor.datacollection.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @ClassName CorsConfig
+ * @Dessription TODO
+ * @Author 杨丰畅
+ * @Date 2019/10/25 16:27
+ **/
+@Configuration
+public class CorsConfig {
+    @Value("${url.frontend.domain}")
+    private String url;
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置你要允许的网站域名，如果全允许则设为 *
+        config.addAllowedOrigin("*");
+        // 如果要限制 HEADER 或 METHOD 请自行更改
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        // 这个顺序很重要哦，为避免麻烦请设置在最前
+        bean.setOrder(0);
+        return bean;
+    }
+}
